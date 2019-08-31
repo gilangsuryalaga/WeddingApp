a906f0ab5065c57e07fd7846fe97f04e2277f479
gilangsuryalaga <gilangsuryalaga@gmail.com>
2019-08-31 22:06:35 +0700

    mantul

--- app/src/main/java/com/example/weddingapp/MainActivity.java
+++ app/src/main/java/com/example/weddingapp/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.weddingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
-
+//SearchView mySearchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mySearchView = (SearchView) findViewById(R.id.searchview);
     }
 }
